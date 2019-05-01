d99c5b2489e9795091ac4d4400b366bb62e9cfe4
Bobbi <lefatshe70@yahoo.com>
2019-05-01 23:59:27 +0200

    added menu tabs

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 
-console.log("Self executing function here");
+function on() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
+  
 (function() {
     var tiny = $('#tiny').tiny().data('api_tiny');
  })();
--- js/vendor/tinyslide.js
+++ js/vendor/tinyslide.js
@@ -331,7 +331,7 @@
         return;
       }
 
-      var output = "<footer class='footer'> <div class='footer__controls'> <div class='next_link'>  <button><span data-hover='Next'>Next</span></button> </div> <div class='pagination'><ul>\n";
+      var output = "<footer class='footer'> <div class='footer__controls'> <div class='next_link'>  <button><span data-hover='ONLINE'>BOOKING </span></button> </div> <div class='pagination'><ul>\n";
       for(var i=0; i < _.numSlides; i++){
         output += "<li class='' data-index='"+i+"'><a style='transition-duration: 7000ms'></a></li>\n";
       }
