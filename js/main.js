03cb0390cd75f4633a80f2f5f350ae7a84b09fbb
Sechaba Mothobi <sechaba.mothobi@investec.co.za>
2019-05-10 21:48:20 +0200

    mobile optimize

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@
 function on() {
     document.getElementById("overlay").style.display = "block";
   }
-  
+
   function off() {
     document.getElementById("overlay").style.display = "none";
   }
-  
+
 (function() {
 
 // Count down
@@ -24,18 +24,32 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
   // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-  // If the count down is over, write some text 
+    var daysVar = "<div class='li'>" + days + "<small> days </small></div>";
+    var hoursVar = "<div class='li'>" +  hours + "<small> hours </small></div>";
+    var minVar = "<div class='li'>" + minutes + "<small> minutes </small></div>";
+    var secsVar = "<div class='li'>" + seconds + "<small> seconds </small></div>";
+  document.getElementById("demo").innerHTML = "<div class='count-t'>" + daysVar + hoursVar + minVar + secsVar + "</div>";
+
+  var width = $(window).width();
+  if (width <= 380) {
+    $('body').addClass('mobi-friendly');
+    document.getElementById("mobile-count").innerHTML = "<div class='count-t'>" + daysVar + hoursVar + minVar + secsVar + "</div>";
+  } else {
+    $('body').removeClass('mobi-friendly');
+  }
+
+  // If the count down is over, write some text
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("demo").innerHTML = "EXPIRED";
   }
 }, 1000);
-  
+
     var tiny = $('#tiny').tiny().data('api_tiny');
-  
-    $(window).on('load', function(){ 
-      $(".se-pre-con").hide() 
+
+    $(window).on('load', function(){
+      $(".se-pre-con").fadeOut();
+        $(".se-pre-txt").fadeOut()
     });
 
  })();
